94cd433c2ab5cc273f4604afea9d6af24e1569c8
CoderPlayer0 <2997385876@qq.com>
2021-01-12 15:32:54 +0800

    [20210112]初始化项目，增加echarts和个人页面

---
+++ src/components/demo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="demo">
+    顶部页面
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'demo'
+}
+</script>
+
+<style>
+.demo{
+  width: 100%;
+  height: 200px;
+  background-color: #42B983;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/echart',
+      name: 'echart',
+      component: () => import("@/components/echart")
+    },
+    {
+      path: '/demo',
+      name: 'demo',
+      component: () => import("@/components/demo")
+    }
+  ]
+})
