c968e64b3937116860629604632ff763ffd3bc17
CoderPlayer0 <2997385876@qq.com>
2021-01-15 11:45:15 +0800

    [202101151144]增加elementui学习代码

--- src/components/demo.vue
+++ src/components/demo.vue
@@ -1,19 +1,67 @@
 <template>
   <div class="demo">
-    顶部页面
+    <el-container>
+      <el-header>
+        <el-menu :default-active="activeIndex2" class="el-menu-demo" mode="horizontal" @select="handleSelect"
+          background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
+          <el-menu-item index="处理中心">处理中心</el-menu-item>
+          <el-submenu index="2">
+            <template slot="title">我的工作台</template>
+            <el-menu-item index="我的工作台-选项1">选项1</el-menu-item>
+            <el-submenu index="2-4">
+              <template slot="title">选项2</template>
+              <el-menu-item index="我的工作台-选项2-选项1">选项1</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+          <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
+        </el-menu>
+      </el-header>
+      <el-main>{{value}}</el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'demo'
-}
+  export default {
+    name: 'demo',
+    data() {
+      return {
+        activeIndex: '1',
+        activeIndex2: '1',
+        value: 'Main'
+      }
+    },
+    methods: {
+      handleSelect(key, keyPath) {
+        this.value = key
+      }
+    }
+  }
 </script>
 
 <style>
-.demo{
-  width: 100%;
-  height: 200px;
-  background-color: #42B983;
-}
+  html,
+  body,
+  #app,
+  .demo,
+  .el-container {
+    height: 100%;
+  }
+
+  .el-header,
+  .el-footer {
+    padding: 0;
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
 </style>
